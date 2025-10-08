1c3e14e622eb30153062c64dd5f67c8822144c04
leothaylor <132721431+leothaylor@users.noreply.github.com>
2025-10-08 02:51:40 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// Preencha com o seu projeto (Supabase → Settings → API)
+// Supabase → Settings → API
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://SEU-PROJECT-REF.supabase.co",
-  SUPABASE_ANON_KEY: "SEU-ANON-KEY"
+  SUPABASE_URL: "https://ttchylymyftizjsqbngp.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0Y2h5bHlteWZ0aXpqc3FibmdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4OTMyMDMsImV4cCI6MjA3NTQ2OTIwM30.iSYuaKZDcWnW2mzgSgdPoRY0dyY7A_fufEvYCBaHcEI"
 };
